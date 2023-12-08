75eaa4a82a75dd7c8b65fe5896f38958e5322431
LIM ZHONG PENG <zplim9338@gmail.com>
2023-12-08 19:27:12 +0800

    initial maven java spring boot project

---
+++ src/main/java/com/example/codeanything/CodeanythingApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeanything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeanythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeanythingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeanything/CodeanythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeanything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeanythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
