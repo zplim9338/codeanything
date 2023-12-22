6b0c32d13380cafd15f70c66168045d1fc66c2cf
LIM ZHONG PENG <zplim9338@gmail.com>
2023-12-22 21:28:38 +0800

    Global | Customize Exception Handler to use ApiResponse Format

---
+++ src/main/java/com/anything/codeanything/modules/user/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.anything.codeanything.modules.user.exception;
+import com.anything.codeanything.modules.user.model.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+        ApiResponse<Void> response = new ApiResponse<>();
+        response.setStatus(false);
+        response.setMessage("An error occurred: " + e.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
